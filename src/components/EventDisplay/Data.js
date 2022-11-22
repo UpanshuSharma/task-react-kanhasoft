28d372d51e496e022fe7dc17c9aa461d4ee2822a
Upanshu Sharma <upanshupathak@gmail.com>
2022-11-22 08:27:32 +0530

    Dummy data file created

---
+++ src/components/EventDisplay/Data.js
@@ -0,0 +1,45 @@
+export const data = [
+  {
+    name: "task1",
+    type: "school",
+    startDate: "178/830",
+    endDate: "71/28/20",
+    handle: "Upanshu",
+    organization: "ABRC",
+    subevent: 10,
+    desc: "This is Description",
+  },
+
+  {
+    name: "task1",
+    type: "school",
+    startDate: "178/830",
+    endDate: "71/28/20",
+    handle: "Upanshu",
+    organization: "ABRC",
+    subevent: 10,
+    desc: "This is Description",
+  },
+
+  {
+    name: "task1",
+    type: "school",
+    startDate: "178/830",
+    endDate: "71/28/20",
+    handle: "Upanshu",
+    organization: "ABRC",
+    subevent: 10,
+    desc: "This is Description",
+  },
+
+  {
+    name: "task1",
+    type: "school",
+    startDate: "178/830",
+    endDate: "71/28/20",
+    handle: "Upanshu",
+    organization: "ABRC",
+    subevent: 10,
+    desc: "This is Description",
+  },
+];
